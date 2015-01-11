c5402df10f30f6f2096224636dc362ffb0a79c3f
Brian Sam-Bodden <bsbodden@integrallis.com>
2015-01-11 08:57:40 -0700

    Updated for Drools 6.1

--- src/main/java/org/integrallis/drools/DroolsTest.java
+++ src/main/java/org/integrallis/drools/DroolsTest.java
@@ -1,56 +1,30 @@
 package org.integrallis.drools;
 
 import org.apache.log4j.Logger;
-import org.drools.KnowledgeBase;
-import org.drools.KnowledgeBaseFactory;
-import org.drools.builder.KnowledgeBuilder;
-import org.drools.builder.KnowledgeBuilderError;
-import org.drools.builder.KnowledgeBuilderErrors;
-import org.drools.builder.KnowledgeBuilderFactory;
-import org.drools.builder.ResourceType;
-import org.drools.io.ResourceFactory;
-import org.drools.logger.KnowledgeRuntimeLogger;
-import org.drools.logger.KnowledgeRuntimeLoggerFactory;
-import org.drools.runtime.StatefulKnowledgeSession;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 
 public class DroolsTest {
 	
 	private static Logger applicationLogger = Logger.getLogger(DroolsTest.class);
 
 	public static final void main(String[] args) {
-		try {
-			// load up the knowledge base
-			KnowledgeBase knowledgeBase = readKnowledgeBase();
-			StatefulKnowledgeSession knowledgeSession = knowledgeBase.newStatefulKnowledgeSession();
+	    KieSession knowledgeSession = null;
+	    try {
+	        // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+		    KieContainer kContainer = ks.getKieClasspathContainer();
+		    knowledgeSession = kContainer.newKieSession("ksession-rules");
 			
-			knowledgeSession.setGlobal("myLogger", applicationLogger);
-			
-			KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(knowledgeSession, "test");
-			
+		    knowledgeSession.setGlobal("applicationLogger", applicationLogger);
 			knowledgeSession.insert(new SomeClass("Hey you!"));
 
 			knowledgeSession.fireAllRules();
-			logger.close();
 		} catch (Throwable t) {
 			t.printStackTrace();
-		}
+		} finally {
+			knowledgeSession.dispose();
+        }
 	}
-
-	private static KnowledgeBase readKnowledgeBase() throws Exception {
-		KnowledgeBuilder knowledgeBuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-		knowledgeBuilder.add(ResourceFactory.newClassPathResource("sample.drl"), ResourceType.DRL);
-		KnowledgeBuilderErrors errors = knowledgeBuilder.getErrors();
-		if (errors.size() > 0) {
-			for (KnowledgeBuilderError error: errors) {
-				System.err.println(error);
-			}
-			throw new IllegalArgumentException("Could not parse knowledge.");
-		}
-		KnowledgeBase knowledgeBase = KnowledgeBaseFactory.newKnowledgeBase();
-		knowledgeBase.addKnowledgePackages(knowledgeBuilder.getKnowledgePackages());
-		return knowledgeBase;
-	}
-	
-	
-
 }
